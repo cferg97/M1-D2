a5edf83f0abf364dbd7d930993e1862eb5c68968
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-12-20 14:42:08 +0000

    Working GET/PUT/DELETE

---
+++ src/authors/index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import uniqid from "uniqid";
+
+const authorsJSONpath = join(
+  dirname(fileURLToPath(import.meta.url)),
+  "authors.json"
+);
+
+const authorsRouter = express.Router();
+
+authorsRouter.post("/", (req, res) => {
+  const newAuthor = {
+    ...req.body,
+    createdAt: new Date(),
+    id: uniqid(),
+    avatar: `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`,
+  };
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONpath));
+  authorsArray.push(newAuthor);
+  fs.writeFileSync(authorsJSONpath, JSON.stringify(authorsArray));
+  res.status(201).send({ id: newAuthor.id });
+});
+
+authorsRouter.get("/", (req, res) => {
+  const fileContent = fs.readFileSync(authorsJSONpath);
+  const authors = JSON.parse(fileContent);
+  res.send(authors);
+});
+
+authorsRouter.get("/:authorId", (req, res) => {
+  const authorId = req.params.authorId;
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONpath));
+  const foundAuthor = authorsArray.find((user) => user.id === authorId);
+  res.send(foundAuthor);
+});
+
+authorsRouter.put("/:authorId", (req, res) => {
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONpath));
+  const index = authorsArray.findIndex(
+    (author) => author.id === req.params.authorId
+  );
+  const oldAuthor = authorsArray[index];
+
+  const updatedAuthor = {
+    ...oldAuthor,
+    ...req.body,
+    updatedAt: new Date(),
+    avatar: `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`,
+  };
+
+  authorsArray[index] = updatedAuthor;
+
+  fs.writeFileSync(authorsJSONpath, JSON.stringify(authorsArray));
+  res.send(updatedAuthor);
+});
+
+authorsRouter.delete("/:authorId", (req, res) => {
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONpath));
+  const remainingAuthors = authorsArray.filter(
+    (author) => author.id !== req.params.authorId
+  );
+  fs.writeFileSync(authorsJSONpath, JSON.stringify(remainingAuthors));
+
+  res.status(204).send("Deleted.");
+});
+
+export default authorsRouter;
---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import authorsRouter from "./authors/index.js";
+
+const server = express();
+const port = 3001;
+
+server.use(express.json());
+
+server.use("/authors", authorsRouter);
+
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
